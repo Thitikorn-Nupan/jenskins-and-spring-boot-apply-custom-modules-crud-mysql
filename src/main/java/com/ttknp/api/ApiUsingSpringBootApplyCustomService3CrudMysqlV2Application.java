8124594b71955f1d61f5991fa9eb68af9bc5b571
thitikorn_nupan@outlook.co.th <thitikorn_nupan@outlook.co.th>
2025-12-08 11:46:12 +0700

    commit

--- src/main/java/com/ttknp/api/ApiUsingSpringBootApplyCustomService3CrudMysqlV2Application.java
+++ src/main/java/com/ttknp/api/ApiUsingSpringBootApplyCustomService3CrudMysqlV2Application.java
@@ -6,9 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan(basePackages = {"com.ttknp"})
-@SpringBootApplication// (scanBasePackages = "com.ttknp")// it's same @ComponentScan(basePackages = {"com.ttknp"}) // *** if you gonna test (with mockito) you have to comment this annotation
+@SpringBootApplication// (scanBasePackages = "com.ttknp") it's same @ComponentScan(basePackages = {"com.ttknp"}) ??? // *** if you gonna test (with mockito) you have to comment this annotation
 public class ApiUsingSpringBootApplyCustomService3CrudMysqlV2Application {
-
 
     public static void main(String[] args) {
         SpringApplication.run(ApiUsingSpringBootApplyCustomService3CrudMysqlV2Application.class, args);
