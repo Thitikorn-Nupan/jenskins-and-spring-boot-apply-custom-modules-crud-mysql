db5d4a477f78d7f338fb9c042994292b06909766
thitikorn_nupan@outlook.co.th <thitikorn_nupan@outlook.co.th>
2025-12-08 10:10:16 +0700

    commit

--- src/main/java/com/ttknp/api/ApiUsingSpringBootApplyCustomService3CrudMysqlV2Application.java
+++ src/main/java/com/ttknp/api/ApiUsingSpringBootApplyCustomService3CrudMysqlV2Application.java
@@ -2,10 +2,9 @@ package com.ttknp.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan(basePackages = "com.ttknp")
-@SpringBootApplication
+
+@SpringBootApplication(scanBasePackages = "com.ttknp")// it's same @ComponentScan(basePackages = {"com.ttknp"}) // *** if you gonna test (with mockito) you have to comment this annotation
 public class ApiUsingSpringBootApplyCustomService3CrudMysqlV2Application {
 
     public static void main(String[] args) {
--- src/main/java/com/ttknp/api/dto/RomanceDTO.java
+++ src/main/java/com/ttknp/api/dto/RomanceDTO.java
@@ -4,9 +4,11 @@ import com.ttknp.api.entity.Romance;
 import com.ttknp.api.repository.ModelRepository;
 import com.ttknp.jdbccustomservice.jdbc.select.JdbcSelectHelper;
 import com.ttknp.jdbccustomservice.jdbc.update.JdbcInsertUpdateDeleteHelper;
+import com.ttknp.jdbccustomservice.jdbc.utility.JdbcReadSQLFileHelper;
 import com.ttknp.valiadationcustomservice.validation.UtilityStringHelper;
 import com.ttknp.valiadationcustomservice.validation.ValidateHelperService;
 import com.ttknp.webcustomservice.exception.ContentNotAllowed;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
@@ -16,11 +18,20 @@ public class RomanceDTO implements ModelRepository<Romance> {
 
     private final JdbcSelectHelper<Romance> jdbcSelectHelper;
     private final JdbcInsertUpdateDeleteHelper<Romance> jdbcInsertUpdateDeleteHelper;
+    private final JdbcReadSQLFileHelper jdbcReadSQLFileHelper;
 
     @Autowired
-    public RomanceDTO(JdbcSelectHelper<Romance> jdbcSelectHelper, JdbcInsertUpdateDeleteHelper<Romance> jdbcInsertUpdateDeleteHelper) {
+    public RomanceDTO(JdbcSelectHelper<Romance> jdbcSelectHelper, JdbcInsertUpdateDeleteHelper<Romance> jdbcInsertUpdateDeleteHelper, JdbcReadSQLFileHelper jdbcReadSQLFileHelper) {
         this.jdbcSelectHelper = jdbcSelectHelper;
         this.jdbcInsertUpdateDeleteHelper = jdbcInsertUpdateDeleteHelper;
+        this.jdbcReadSQLFileHelper = jdbcReadSQLFileHelper;
+    }
+
+
+    //The @PostConstruct annotation in Java marks a method to be run after dependency injection is complete but before the bean goes into service,
+    @PostConstruct
+    private void initService() {
+        jdbcReadSQLFileHelper.loadScriptRootPath("sql/init_db.sql");
     }
 
     @Override
